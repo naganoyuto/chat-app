0789325bbd6e98d015f23d04462a1369e402059f
singori <naganoyuto@icloud.com>
2021-02-08 19:19:25 +0900

    メッセージ送信機能の実装

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,4 @@
-class Messsage < ApplicationRecord
+class Message < ApplicationRecord
     belongs_to :room
     belongs_to :user
 end
--- db/migrate/20210208094329_create_messages.rb
+++ db/migrate/20210208094329_create_messages.rb
@@ -1,6 +1,6 @@
-class CreateMesssages < ActiveRecord::Migration[6.0]
+class CreateMessages < ActiveRecord::Migration[6.0]
   def change
-    create_table :messsages do |t|
+    create_table :messages do |t|
       t.string  :content
       t.references :room, foreign_key: true
       t.references :user, foreign_key: true
