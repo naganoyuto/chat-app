cf0573e5c5e082b5a663d919c0ec44130f91391f
singori <naganoyuto@icloud.com>
2021-02-08 18:09:00 +0900

    messagesテーブルの作成

---
+++ app/models/messsage.rb
@@ -0,0 +1,4 @@
+class Messsage < ApplicationRecord
+    belongs_to :room
+    belongs_to :user
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_08_055151) do
+ActiveRecord::Schema.define(version: 2021_02_08_090213) do
+
+  create_table "messsages", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "content"
+    t.bigint "room_id"
+    t.bigint "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["room_id"], name: "index_messsages_on_room_id"
+    t.index ["user_id"], name: "index_messsages_on_user_id"
+  end
 
   create_table "room_users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "room_id"
@@ -40,6 +50,8 @@ ActiveRecord::Schema.define(version: 2021_02_08_055151) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "messsages", "rooms"
+  add_foreign_key "messsages", "users"
   add_foreign_key "room_users", "rooms"
   add_foreign_key "room_users", "users"
 end
